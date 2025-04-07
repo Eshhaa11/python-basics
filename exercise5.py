58ec6ed0d6287086668ea03bac3519fefca66882
Esha Patel <Patellesha11@gmail.com>
2025-04-07 17:26:37 +0300

    added if and else

--- exercise5.py
+++ exercise5.py
@@ -4,6 +4,6 @@ num2 = int(input('Enter the second number: '))
 product = num1 * num2
 
 if product <= 1000:
-    print("The product is, product")
+    print("The product is", product)
 else:
-    print("The sum is, num1 + num2")
+    print("The sum is", num1 + num2)
