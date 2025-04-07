d71052db8a200e425bc52f60295992aa2ac29d83
Esha Patel <Patellesha11@gmail.com>
2025-04-07 17:13:02 +0300

    added elif

--- exercise3.py
+++ exercise3.py
@@ -2,7 +2,8 @@ weather =input("What is the weather like today?")
 
 if weather == "sunny":
     print ("Wear Sunglasses 😎")
-if weather == "rainy": 
+elif weather == "rainy": 
       print ("Take an Umbrella ☔")
 if weather == "colde":
        print ("Wear a jacket 🧥")
+
